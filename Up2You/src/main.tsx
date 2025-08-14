98feab7458a857b2ebff90a554bfca5fa8c231db
AmazonAlexa001 <154873101+AmazonAlexa001@users.noreply.github.com>
2025-08-14 02:50:08 -0700

    Update main.tsx

--- Up2You/src/main.tsx
+++ Up2You/src/main.tsx
@@ -4,7 +4,7 @@
 import from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
