637c5f8606c04b960cf170a30fe340dd6d19e33f
AmazonAlexa001 <154873101+AmazonAlexa001@users.noreply.github.com>
2025-08-14 02:47:05 -0700

    Update main.tsx

--- Up2You/src/main.tsx
+++ Up2You/src/main.tsx
@@ -1,7 +1,7 @@
 // ─────────────────────────────────────────────────────────────────────────────
 // File: src/main.tsx
 // ─────────────────────────────────────────────────────────────────────────────
-import React from "react";
+import from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
