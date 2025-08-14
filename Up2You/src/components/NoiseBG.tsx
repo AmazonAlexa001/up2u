d4bfa01620b812e5529c387cca91c806df3e5acd
AmazonAlexa001 <154873101+AmazonAlexa001@users.noreply.github.com>
2025-08-14 02:53:22 -0700

    Update NoiseBG.tsx

--- Up2You/src/components/NoiseBG.tsx
+++ Up2You/src/components/NoiseBG.tsx
@@ -1,5 +1,4 @@
 // ─────────────────────────────────────────────────────────────────────────────
 // File: src/components/NoiseBG.tsx
 // ─────────────────────────────────────────────────────────────────────────────
-import React from "react";
 export default function NoiseBG(){ return <div aria-hidden className="bg-grain" />; }
